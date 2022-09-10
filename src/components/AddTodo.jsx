68991b6f56759bbdb1bdebab774d1ae70700c284
Furkan <furkan.gundz00@gmail.com>
2022-09-10 22:25:31 +0300

    Create custom input.

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../style/todoInput.css';
+
+function AddTodo({ value, onChange, onSubmit }) {
+	return (
+		<div className='addTodo'>
+			<div className='form_group'>
+				<input
+					type='input'
+					className='form_field'
+					placeholder='What to do today ?'
+					name='newtodo'
+					id='newtodo'
+					onChange={onChange}
+					value={value}
+				/>
+				<label htmlFor='newtodo' className='form_label'>
+					What to do today ?
+				</label>
+			</div>
+			<div>
+				<button onClick={onSubmit}>Submit</button>
+			</div>
+		</div>
+	);
+}
+
+export default AddTodo;
