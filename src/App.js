2b837ab08b4fbffa4999ddacd6ebd19cdcfa76a7
Furkan <furkan.gundz00@gmail.com>
2022-09-10 23:29:25 +0300

    Added react-toastify for toast messages.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import AddTodo from "./components/AddTodo";
-
+import TodoList from "./components/TodoList";
+import { toast } from "react-toastify"
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -23,6 +24,10 @@ function App() {
   }, [])
 
   const onSubmit = async () => {
+    if (newTodo.content.trim() == "") {
+      toast.error("Input required.")
+      return
+    }
     await fetch(
       process.env.REACT_APP_API_ENDPOINT + "todos", {
       headers: {
@@ -32,9 +37,11 @@ function App() {
       body: JSON.stringify(newTodo),
     }).then(
       (response) => response.json()
+
     ).then(data => {
-      console.log("Succes.", data)
+      console.log("Succes.")
       getDataFromApi()
+      setInputEmpty()
 
     }).catch(error => {
       console.log("Error.", error)
@@ -45,17 +52,15 @@ function App() {
     setNewTodo((prev) => ({ ...prev, "content": e.target.value }))
   }
 
+  const setInputEmpty = () => {
+    setNewTodo((prev) => ({ ...prev, "content": "" }))
+  }
+
   return (
     <main>
       <Header />
       <AddTodo value={newTodo.content} onChange={onChange} onSubmit={onSubmit} />
-
-
-      <ul>
-        {todos.map((todo) =>
-          <li key={todo.id}> {todo.content} </li>
-        )}
-      </ul>
+      <TodoList todos={todos} />
     </main>
   );
 }
